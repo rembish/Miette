1baa85e64f72c30603d915ccaca9ee7430d82c75
Aleksey Rembish <alex@rembish.ru>
2010-11-30 21:13:21 +0100

    First draft variant of microsoft word document (.doc) reader

--- src/doc/text.py
+++ src/doc/text.py
@@ -1,4 +1,5 @@
 from cfb.reader import Reader
+from struct import unpack
 
 class DocTextReader(Reader):
     def __init__(self, filename):
@@ -20,3 +21,72 @@ class DocTextReader(Reader):
             self._n_table = self.get_entry_by_name(n_table_name)
 
         return self._n_table
+
+    def get_raw_text(self):
+        self.word_document.seek(0x004c)
+        (ccp_text, ccp_ftn, ccp_hdd, ccp_mcr, ccp_atn, ccp_edn, ccp_txbx, \
+            ccp_hdr_txbx) = unpack('<LLLLLLLL', self.word_document.read(32))
+
+        last_cp = ccp_ftn + ccp_hdd + ccp_mcr + ccp_atn + ccp_edn + ccp_txbx \
+            + ccp_hdr_txbx
+        last_cp += (0 if not last_cp else 1) + ccp_text
+
+        pos = fc_clx = self.word_document.get_long(0x01a2)
+        lcb_clx = self.word_document.get_long(0x01a6)
+        clxt = self.n_table.get_byte(fc_clx)
+
+        if clxt == 0x01:
+            pos += 1
+            cb_grpprl = self.n_table.get_short(pos)
+
+            pos += 2
+            if cb_grpprl > 0x3fa2:
+                raise Exception()
+
+            pos += cb_grpprl
+            clxt = self.n_table.get_byte(pos)
+
+        if clxt == 0x02:
+            pos += 1
+            lcb = self.n_table.get_long(pos)
+
+            fc_plc_pcd = pos + 4
+            if lcb != lcb_clx - 5:
+                raise Exception()
+        else:
+            raise Exception()
+
+        cp = []
+        self.n_table.seek(fc_plc_pcd)
+        for i in range(0, lcb_clx - (fc_plc_pcd - fc_clx), 4):
+            cp.append(unpack('<L', self.n_table.read(4))[0])
+            if cp[-1] == last_cp:
+                break
+
+        if not len(cp) or cp[-1] != last_cp:
+            raise Exception()
+
+        parts = []
+        for i in range(0, lcb_clx - (self.n_table.tell() - fc_clx), 8):
+            (abc_fr2, fc, prm) = unpack('<HLH', self.n_table.read(8))
+            fc_f_compressed = (fc & 40000000) == 40000000
+            fc_fc = fc & 0x3fffffff
+
+            j = i / 8
+            length = cp[j + 1] - cp[j]
+            if fc_f_compressed:
+                fc_fc /= 2
+            else:
+                length *= 2
+
+            parts.append((fc_fc, length, fc_f_compressed))
+
+        buffer = ""
+        for (start, length, compressed) in parts:
+            self.word_document.seek(start)
+            if not compressed:
+                buffer += self.word_document.read(length).decode('utf-16')
+            else:
+                buffer += self.word_document.read(length)
+
+        return buffer.encode('utf-8')
