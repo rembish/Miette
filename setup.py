8fe1445a06a308a63fd4dce792bd33d9f4487d5e
Jason Brechin <jbrechin@enclarity.com>
2013-06-13 11:27:53 -0500

    Fill in setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,45 @@
-__author__ = 'jbrechin'
+#!/usr/bin/env python
+import os
+import sys
+import miette as miette
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+packages = [
+    'miette',
+    'miette.doc',
+    'miette.cfb',
+    'miette.tools'
+]
+
+requires = []
+
+setup(
+    name='miette',
+    version=miette.__version__,
+    description='Miette is a light-weight Microsoft Office documents reader',
+    author='Alex Rembish',
+    author_email='alex@rembish.ru',
+    packages=packages,
+    #package_data={'': ['LICENSE'],},
+    package_dir={'miette': 'miette'},
+    include_package_data=True,
+    install_requires=requires,
+    #license=open('LICENSE').read(),
+    zip_safe=False,
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ),
+)
